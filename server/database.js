02c929a5105f70ee1564ff5761c1fb0a63114b1d
mtopwav <mtopwav@gmail.com>
2026-07-10 14:47:40 +0300

    update db name

--- server/database.js
+++ server/database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "mamuya_dsm",
+  database: process.env.DB_NAME || "mamuya_dsm_db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
